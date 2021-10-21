145c2227d4d958c5cdef2bae1264ff53350ec6b3
Yaroslav quetalse <quetalse@gmail.com>
2021-10-22 00:56:56 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
+import InlineEdit from "inline-edit-react";
+import logo from './logo.png';
 import './App.css';
+
+console.log('InlineEdit', InlineEdit)
+
+const Title = ({title, asyncEditHandler}) => {
+
+        console.log(title, asyncEditHandler)
+
+        let onSuccess = () => {
+            alert('ok');
+        };
+
+        let onFail = (err) => {
+            alert(`error: ${err}`);
+        }
+
+        let onEdit = (newTitle) => {
+            asyncEditHandler(newTitle)
+        }
+ 
+        return (
+            <InlineEdit 
+                initTitle = {title}
+                onEdit = {asyncEditHandler}
+                onSuccess = {onSuccess}
+                onFail = {onFail}
+                align = 'center' // ['left', 'center', 'right']
+            >
+            </InlineEdit>
+        )
+  
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style={{filter: 'invert(100%)'}}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <i>title</i> below to test.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p style={{marginTop: '30px', width: '400px'}}>
+              <Title  title="inline-edit-react" asyncEditHandler={(title) => {console.log(title)}}/>
+          </p>
       </header>
     </div>
   );
---
+++ src/components/index.js
@@ -0,0 +1,149 @@
+import React, {useState, Fragment} from 'react';
+import Loader from "./loader/loader";
+import PropTypes from 'prop-types';
+import './index.css';
+
+const InputMode = ({title, setTitle, dropTitle, updateHandler}) => {
+
+    const changeHandler = (e) => {
+        setTitle(e.target.value)
+    };
+
+    const keyDownHandler = (event) => {
+        if(event.key === 'Enter'){
+            updateHandler(title);
+        }
+        if (event.key === 'Escape'){
+            dropTitle();
+        }
+    }
+
+    return (
+        <input
+            className="input-mode"
+            type="text"
+            value={title}
+            // style={{...style}}
+            onChange={changeHandler}
+            onBlur={dropTitle}
+            autoFocus={true}
+            onKeyDown={keyDownHandler}
+        />
+    )
+}
+
+const TextMode = ({enableEditable, title, loading}) => {
+
+    const loader = loading && <Loader/>
+    const setBackground = (e) => {
+        e.target.style.background = '#e6e4e4';
+        e.target.style.color = '#212121';
+        e.target.style.borderRadius = '8px';
+        e.target.style.padding = '0 6px';
+    }
+    const clearBackground = (e) => {
+        e.target.style.background = 'none';
+        e.target.style.color = 'inherit';
+    }
+
+    return (
+        <Fragment>
+            <span
+                className="text-mode"
+                onMouseOver={setBackground}
+                onMouseLeave={clearBackground}
+                onClick={enableEditable}
+            >
+                {title}
+            </span>
+            {loader}
+        </Fragment>
+    )
+}
+
+const InlineEdit = ({initTitle, onEdit, onSuccess, onFail, align}) => {
+
+    const [isEditable, setEditable] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [title, setTitle] = useState(initTitle);
+    const titleAlign = () => {
+        switch(align){
+            case "center":
+                return "ier-center"
+            case "left":
+                return "ier-left"
+            case "right":
+                return "ier-right"
+            default:
+                return "ier-center"
+        }
+    }
+
+    const enableEditable = () => {
+        setEditable(true);
+    };
+    const disableEditable = () => {
+        setEditable(false);
+    };
+
+    const dropTitle = () => {
+        setTitle(initTitle);
+        disableEditable();
+    }
+
+    const updateHandler = async (newData) => {
+        if(newData.trim() === initTitle){
+            dropTitle()
+        }else{
+            disableEditable();
+            setLoading(true);
+            try{
+                await onEdit(newData);
+                onSuccess();
+                setLoading(false);
+            }
+            catch (err) {
+                onFail(err);
+                dropTitle();
+                setLoading(false);
+            }
+            setTitle(newData);
+        }
+    };
+
+    const mode = isEditable ?
+        <InputMode
+            title={title}
+            setTitle={setTitle}
+            dropTitle={dropTitle}
+            updateHandler={updateHandler}
+        /> :
+        <TextMode
+            enableEditable={enableEditable}
+            title={title}
+            loading={loading}
+        />
+
+    return (
+        <div className={`wrapper ${titleAlign()}`}>
+            {mode}
+        </div>
+    );
+}
+
+
+InlineEdit.propTypes = {
+    initTitle: PropTypes.string.isRequired,
+    onEdit: PropTypes.func.isRequired,
+    align: PropTypes.oneOf(['center', 'left', 'right'])
+}
+
+InlineEdit.defaultProps = {
+    initTitle: '',
+    onSuccess: () => {return true},
+    onFail: () => {return true}
+}
+
+
+
+export default InlineEdit;
